26635ed9c9dc17a6189f5a768d4b5f5dc391f188
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-03-04 09:16:59 +0100

    Update updateservice.bat

--- app/updateservice.bat
+++ app/updateservice.bat
@@ -16,7 +16,7 @@ rmdir /S /Q C:\PAPERSTUDIOS\Data\%inproname%\versions
 cls
 echo.
 echo  [i] FALLS FEHLER IN DER NEUEN VERSION AUFTRETEN,
-echo      KOENNEN SIE DIE IN UPAAI UNTER FORMS MELDEN
+echo      KOENNEN SIE DIE IN UPAI UNTER FORMS MELDEN
 PING localhost -n 6 >NUL
 if exist "archiver\WinRAR.exe" (
 "archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\APPUPDATE.zip" *.* C:\PAPERSTUDIOS\Data\%inproname%\
